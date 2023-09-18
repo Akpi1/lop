6a83b8c91deec864dfbb80cd7fe758da1abc11ec
chidi arinze <86808937+Akpi1@users.noreply.github.com>
2023-09-19 00:00:30 +0100

    Add files via upload

---
+++ LOP/scripts/word-list.js
@@ -0,0 +1,22 @@
+const wordList = [
+    {
+        word: "legends Of The Past",
+        hint: "Mobile game combines historical characters, reimagined for the battle arena"
+    },
+    {
+        word: "heavenkeys",
+        hint: "Won through exciting quests on Zealy."
+    },
+    {
+        word: "hedera",
+        hint: "Blockchain that LOP was created on."
+    },
+    {
+        word: "cleo",
+        hint: "Has the ancient spirit of Egypt coursing through her veins."
+    },
+    {
+        word: "kim",
+        hint: "The name of LOP arts director."
+    },
+];
